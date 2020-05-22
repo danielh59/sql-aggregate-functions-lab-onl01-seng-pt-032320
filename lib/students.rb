d337ef586e6e32a5215b942783a5f6ea03c0fa0e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-22 08:33:29 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-"SELECT MIN(gpa) FROM name;"
+"SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  SELECT AVG(gpa) FROM name;
+"SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-"  SELECT SUM (tardies) FROM name;"
+"SELECT SUM (tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
-"  SELECT AVG (gpa) FROM name WHERE grade = 9;"
+"SELECT AVG (gpa) FROM students WHERE grade = 9;"
 end
